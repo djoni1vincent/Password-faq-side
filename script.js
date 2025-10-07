215483377e74d2eab4e0724dc3a2de87fbf1ee43
djoni1vincent <75507349+djoni1vincent@users.noreply.github.com>
2025-10-07 13:22:29 +0200

    faq doesn't opening cause duplicating code in js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-const items = document.querySelectorAll(".faq-item");
+document.addEventListener("DOMContentLoaded", () => {
+  const faqItems = document.querySelectorAll(".faq-item");
 
-items.forEach((item) => {
-  const question = item.querySelector(".faq-question");
-  question.addEventListener("click", () => {
-    item.classList.toggle("active");
-  });
-});
-
-const faqItems = document.querySelectorAll(".faq-item");
-
-faqItems.forEach((item) => {
-  item.querySelector(".faq-question").addEventListener("click", () => {
-    item.classList.toggle("active");
+  faqItems.forEach(item => {
+    const question = item.querySelector(".faq-question");
+    question.addEventListener("click", () => {
+      item.classList.toggle("active");
+    });
   });
 });
